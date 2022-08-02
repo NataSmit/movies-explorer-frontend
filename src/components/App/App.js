71928d27036bfdad7064b7a003dcb78f1ca5350e
Natalia Gorobtsova <gorobzova_natalia@mail.ru>
2022-08-03 01:29:15 +0300

    created blocks AboutProject and Techs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import LandingTitle from '../LandingTitle/LandingTitle';
+import Techs from '../Techs/Techs';
 
 
 function App() {
@@ -10,7 +12,12 @@ function App() {
       <Main>
         <Header />
         <Promo />
-        <AboutProject />
+        <AboutProject >
+          <LandingTitle title={'О проекте'}/>
+        </AboutProject>
+        <Techs>
+          <LandingTitle title={'Технологии'}/>
+        </Techs>
       </Main>
 
     </div>
---
+++ src/components/LandingTitle/LandingTitle.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LandingTitle({title}) {
+  return (
+    <h2 className='landingTitle'>{title}</h2>
+  )
+}
